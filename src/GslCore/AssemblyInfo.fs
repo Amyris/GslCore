422aaab3f617213c4344bd2a3c5c63dd8d71c9b4
Chris Macklin <macklin@amyris.com>
2016-12-02 19:30:15 -0800

    VER: update assemblyinfo

--- src/GslCore/AssemblyInfo.fs
+++ src/GslCore/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("GslCore")>]
 [<assembly: AssemblyProductAttribute("GslCore")>]
 [<assembly: AssemblyDescriptionAttribute("Core library and basic plug-ins for the GSL compiler.")>]
-[<assembly: AssemblyVersionAttribute("0.4.4")>]
-[<assembly: AssemblyFileVersionAttribute("0.4.4")>]
+[<assembly: AssemblyVersionAttribute("0.4.5")>]
+[<assembly: AssemblyFileVersionAttribute("0.4.5")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "GslCore"
     let [<Literal>] AssemblyProduct = "GslCore"
     let [<Literal>] AssemblyDescription = "Core library and basic plug-ins for the GSL compiler."
-    let [<Literal>] AssemblyVersion = "0.4.4"
-    let [<Literal>] AssemblyFileVersion = "0.4.4"
+    let [<Literal>] AssemblyVersion = "0.4.5"
+    let [<Literal>] AssemblyFileVersion = "0.4.5"
