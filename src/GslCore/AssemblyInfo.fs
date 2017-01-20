6dd12e59405b6643a4989b877a91e82538555848
platt <platt@amyris.com>
2017-01-19 16:33:46 -0800

    REL: GslCore 0.4.12 published to nuget

--- src/GslCore/AssemblyInfo.fs
+++ src/GslCore/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("GslCore")>]
 [<assembly: AssemblyProductAttribute("GslCore")>]
 [<assembly: AssemblyDescriptionAttribute("Core library and basic plug-ins for the GSL compiler.")>]
-[<assembly: AssemblyVersionAttribute("0.4.11")>]
-[<assembly: AssemblyFileVersionAttribute("0.4.11")>]
+[<assembly: AssemblyVersionAttribute("0.4.12")>]
+[<assembly: AssemblyFileVersionAttribute("0.4.12")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "GslCore"
     let [<Literal>] AssemblyProduct = "GslCore"
     let [<Literal>] AssemblyDescription = "Core library and basic plug-ins for the GSL compiler."
-    let [<Literal>] AssemblyVersion = "0.4.11"
-    let [<Literal>] AssemblyFileVersion = "0.4.11"
+    let [<Literal>] AssemblyVersion = "0.4.12"
+    let [<Literal>] AssemblyFileVersion = "0.4.12"
