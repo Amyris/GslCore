5b15ff8213f3b37679ea46efb98f499ca2cf2a25
Erin Wilson <ewilson@amyris.com>
2017-06-15 22:19:46 +0100

    WIP: compile instead of sourceCompareTest

--- tests/GslCore.Tests/TestL2Expansion.fs
+++ tests/GslCore.Tests/TestL2Expansion.fs
@@ -16,14 +16,15 @@ type TestL2Expansion() =
 
     let phase1WithL2Validation = 
         phase1 Set.empty
-        >=> validateNoAssemlybInL2Promoter
+        >=> validateNoAssemblyInL2Promoter
     
     [<Test>]
     member x.TestDetectAssemblyInL2Promoter() =
         let errorText = "Unsupported use of an Assembly."
         let source = GslSourceCode("(!gERG10 ; !pFBA1 ; pSLN1)>gADH1")
 
-        sourceCompareTest phase1WithL2Validation source source
+        compile phase1WithL2Validation source 
+        |> assertFail L2ExpansionError (Some errorText)
 
 
 //        let tree = lexparse source |> returnOrFail
