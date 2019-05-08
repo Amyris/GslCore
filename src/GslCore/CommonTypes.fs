2bfd4e13d71306649cf6f417ba07c6265085b512
Marcell Legeza <legezam@gmail.com>
2019-05-08 22:28:23 +0200

    FEA: Added #topology pragma

--- src/GslCore/CommonTypes.fs
+++ src/GslCore/CommonTypes.fs
@@ -210,16 +210,17 @@ let recalcOffset (pieces: DNASlice list) =
         {p with destFr = o; destTo = o+(p.dna.Length-1)*1<ZeroOffset> } )
 
 type DnaAssembly =
-   {id: int option;
-    dnaParts: DNASlice list;
-    name: string;
-    uri: Uri option;
-    linkerHint: string;
-    pragmas: PragmaCollection;
-    designParams: DesignParams;
-    docStrings: string list;
-    materializedFrom: Assembly}
-    with
+   { id : int option
+     dnaParts : DNASlice list
+     name : string
+     uri : Uri option
+     linkerHint : string
+     pragmas : PragmaCollection
+     designParams : DesignParams
+     docStrings : string list
+     materializedFrom : Assembly
+     topology : Topology }
+with
     member x.Sequence() =
         x.dnaParts
         |> Seq.map (fun p -> p.dna)
