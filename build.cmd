77b30ce01826291b8907b6f47992a1c91a6e20f5
Marcell Legeza <marcell@demetrixbio.com>
2019-05-14 13:03:11 +0200

    MNT: synchronizing versions with tools

--- build.cmd
+++ build.cmd
@@ -1,15 +1,13 @@
-SET TOOL_PATH=.paket
+SET TOOL_PATH=.tool
 
 IF NOT EXIST "%TOOL_PATH%\paket.exe" (
-  dotnet tool install paket --tool-path ./%TOOL_PATH%
+  dotnet tool install paket --tool-path ./%TOOL_PATH%  --version 5.207.0
 )
 
 "%TOOL_PATH%/paket.exe" restore
 
-SET TOOL_PATH=.fake
-
 IF NOT EXIST "%TOOL_PATH%\fake.exe" (
-  dotnet tool install fake-cli --tool-path ./%TOOL_PATH%
+  dotnet tool install fake-cli --tool-path ./%TOOL_PATH% --version 5.13.7
 )
 
 "%TOOL_PATH%/fake.exe" %*
