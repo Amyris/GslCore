489294998f36f01d279bbc6e9c75437558648f67
Chris Macklin <chris.macklin@gmail.com>
2019-07-22 18:05:02 -0700

    ENH: add a command line argument that globally sets the default reference genome.

--- src/GslCore/CommandConfig.fs
+++ src/GslCore/CommandConfig.fs
@@ -123,6 +123,14 @@ let builtinCmdLineArgs =
              desc = "List all plugins installed in this build of the compiler."}
          proc = fun _ opts -> {opts with listPlugins = true}};
 
+        {spec=
+            {name = "defaultRef"; param = ["refname"]; alias = [];
+             desc = "Globally set the default reference genome."}
+         proc = (fun p opts ->
+             constants.defaultRefGenome <- p.[0]
+             opts)
+        };
+
       
     ]
     |> Seq.map (fun a -> seq {
